246bb4ee501c0a7ae123c68135a77712ca4a02f9
isko18 <toksonbaevislam2004@gmail.com>
2025-12-15 03:45:04 +0600

    firts commit

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -4,6 +4,8 @@ import logging
 import requests
 from django.conf import settings
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 
 from .models import Order
 from .serializers import OrderSerializer
@@ -13,14 +15,21 @@ logger = logging.getLogger(__name__)
 
 
 
+class CsrfExemptSessionAuthentication(SessionAuthentication):
+    def enforce_csrf(self, request):
+        return  # отключаем CSRF для API
+
+
 class OrderCreateView(CreateAPIView):
     """
     POST /orders/
-    Создаёт заказ из корзины (гость или авторизованный), потом шлёт в Telegram.
+    Создаёт заказ (гость или авторизованный), потом шлёт в Telegram.
     """
-
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+
+    permission_classes = [AllowAny]
+    authentication_classes = [CsrfExemptSessionAuthentication, BasicAuthentication]
 
     def perform_create(self, serializer):
         order = serializer.save()
